f79f8c99c3f08151a56aac78b8ae4e603b38193e
Joel Wilson <88785121+joelwilson028@users.noreply.github.com>
2022-03-03 16:25:31 +0530

    Create Dateconvert.py

---
+++ Dateconvert.py
@@ -0,0 +1,8 @@
+def date():
+    months=["jan","feb","mar","ap","ma","ju","jul","ag","sep","oct","nov","dec"]
+    print(months[mon],day,yea)
+day=int(input("day:"))
+mon=int(input("mon:"))
+yea=int(input("yea:"))
+print(mon,"/",day,"/",yea)
+date()
